6e59a285cf83b51fc5d0ee86e766224bd4f56276
Arun <friday99@outlook.it>
2021-03-02 20:20:02 +0100

    added GameRoom UI

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App () {
   return (
     <Background>
       {/* {routeResult || <Home />} */}
-      {routeResult || <FindRooms />}
+      {routeResult || <GameRoom />}
     </Background>
   )
 }
