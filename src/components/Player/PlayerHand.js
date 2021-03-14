3bc4c655f358a62262489b8af08f9584c7daf10c
Jana Rajakumar <jana.rajakumar@utoronto.ca>
2021-03-14 18:11:18 -0400

    Add logic to handle flat vs fan layout

--- src/components/Player/PlayerHand.js
+++ src/components/Player/PlayerHand.js
@@ -3,7 +3,6 @@ import { useState } from 'react'
 import { useLobby } from 'context/LobbyContext'
 
 import {
-  Box,
   Flex,
   Image
 } from '@chakra-ui/react'
@@ -15,41 +14,53 @@ export default function PlayerHand () {
 
   const myCards = lobby.getMyCards()
 
+  const isFanLayout = myCards.length > 10
+
   return (
     <Flex
       w='100%'
-      h='180px'
+      h='200px'
       wrap='wrap'
       position='absolute'
       bottom={0}
       overflow='hidden'
     >
-      <Box
+      <Flex
         width='100%'
         height='100%'
         position='absolute'
+        flexWrap='wrap'
+        justifyContent='center'
       >
-        {myCards.map((card, idx) => (
-          <Image
-            src={card.url}
-            key={card.suite + card.number}
-            alt={`${card.number} of ${card.suite}`}
-            left='50%'
-            top='50%'
-            height='100px'
-            maxHeight='200px'
-            objectFit='contain'
-            position='absolute'
-            filter='drop-shadow(0px 5px 6px rgba(0, 0, 0, 0.25))'
-            transform={`translate(-50%, -50%) rotate(${-rotateCardDegree / 2 + rotateCardDegree / (myCards.length + 1) * (idx + 1)}deg)`}
-            transformOrigin='center 200%'
-            transition='transform 0.3s ease-out'
-            onClick={() => lobby.playCard(card)}
-            onMouseEnter={() => setRotateCardDegree(140)}
-            onMouseLeave={() => setRotateCardDegree(120)}
-          />
-        ))}
-      </Box>
+        {myCards.map((card, idx) => {
+          let fanLayout = {}
+          if (isFanLayout) {
+            fanLayout = {
+              position: 'absolute',
+              transform: `translate(-50%, -50%) rotate(${-rotateCardDegree / 2 + rotateCardDegree / (myCards.length + 1) * (idx + 1)}deg)`
+            }
+          }
+          return (
+            <Image
+              src={card.url}
+              key={card.suite + card.number}
+              alt={`${card.number} of ${card.suite}`}
+              left='50%'
+              top='50%'
+              height='100px'
+              maxHeight='200px'
+              objectFit='contain'
+              {...fanLayout}
+              filter='drop-shadow(0px 5px 6px rgba(0, 0, 0, 0.25))'
+              transformOrigin='center 200%'
+              transition='transform 0.3s ease-out'
+              onClick={() => lobby.playCard(card)}
+              onMouseEnter={() => setRotateCardDegree(140)}
+              onMouseLeave={() => setRotateCardDegree(120)}
+            />
+          )
+        })}
+      </Flex>
     </Flex>
   )
 }
--- src/components/Table.js
+++ src/components/Table.js
@@ -102,7 +102,6 @@ export default function Table () {
   TODO LIST:
   - RESPONSIVE TABLE FOR DESKTOP
   - Table content UI
-  - SHOW FLAT LAYOUT IF CARDS > 10
   - End Game UI
     - ask host to reset game (goes to pre_lobby)
 */
