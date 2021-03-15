11099c9e34f9d1eefa8db68d414afe7a94336432
Jana Rajakumar <jana.rajakumar@utoronto.ca>
2021-03-15 13:22:54 -0400

    Add database url as environment variable

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -5,7 +5,7 @@ import 'firebase/database'
 const firebaseConfig = {
   apiKey: 'AIzaSyCH1OOGPH1daqH03CrW9xZYyGqGwqYQXeg',
   authDomain: 'donkeycardgame.firebaseapp.com',
-  databaseURL: 'https://donkey-dev.firebaseio.com/',
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
   projectId: 'donkeycardgame',
   storageBucket: 'donkeycardgame.appspot.com',
   messagingSenderId: '200596235949',
